2a44a746155e000322ac7dbfb887303a11668852
Gustavo Abramo <112959505+Guh-dev@users.noreply.github.com>
2023-12-02 11:07:08 -0300

    Update pessoa.java

--- pessoa.java
+++ pessoa.java
@@ -1,6 +1,5 @@
 //fazendo uma classe (Pessoa )com seus objetos, no caso os objetos são: nome,email e cpf 
 
-public class pessoa {
     public class Pessoa {
         public String nome;
         public String email;
@@ -8,18 +7,24 @@ public class pessoa {
 
 //Em uma classe, além das características (propriedades) comuns aos objetos, temos também os comportamentos que aquele objeto pode executar. exemplo:
       
-      
         public String exibirDados() {
             return "=== Dados da Pessoa ===" + "\n" +
                     "Nome: " + this.nome + "\n" +
                     "Email: " + this.email + "\n" +
                     "CPF: " + this.cpf;
 
-                    //Então, para a utilização de objetos precisamos definir as classes, por isso alguns defendem que poderíamos chamar de “Programação Orientada a Classes”, o que daria muito certo também
+                    //Então, para a utilização de objetos precisamos definir as classes, por isso alguns defendem que poderíamos chamar de “Programação Orientada a Classes”, o que daria muito certo também.
+                     //Definindo um objeto pessoa.
+                    
 
         }
     }
-}
+                    Pessoa andre = new Pessoa();
+                     andre.nome = "André Silva";
+                     andre.email = "andre@email.com";
+                     andre.cpf = "1111111-11";
+                     System.out.println(andre.exibirDados());
+    
 
 //no mundo real, podemos identificar e classificar diversos objetos que compartilham um conjunto de características em comum. Por exemplo, um livro é um conceito que pode representar vários objetos com características compartilhadas, como capa, autor, número de páginas, ISBN, entre outros; assim, diante de objetos que têm em comum esse conjunto de características, conseguimos classificá-los como livros
 
