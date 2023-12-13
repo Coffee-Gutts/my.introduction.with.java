33a839c9b0dbeb5fafca3fa7af0e85f515739560
Gustavo Abramo <112959505+Coffee-Gutts@users.noreply.github.com>
2023-12-12 23:41:26 -0300

    Add files via upload
    Estudando um dos pilares em java (Herança)

---
+++ Producao.java
@@ -0,0 +1,12 @@
+//HERANÇA: É um mecanismo muito importante na Orientação a Objetos, no qual uma classe pode herdar atributos e comportamentos de uma outra classe. Por meio da herança conseguimos reaproveitar o código, podendo facilitar a implementação e a manutenção de classes no futuro.
+public class Producao {
+        public int id;
+        public String nome;
+        public String classificacao;
+        public String diretor;
+        public String estudio;
+        public int dataLancamento;
+
+    }
+
+//agora vou fazer a classe "serie" herdar da classe produção
---
+++ Serie.java
@@ -0,0 +1,4 @@
+public class Serie extends Producao {
+    public int temporadas;
+    public Date dataEncerramento;
+}
